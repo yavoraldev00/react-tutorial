15d342501476450121fbe3b6c66b49c4976caa23
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-02-18 19:43:13 +0100

    tutorial 8 - using state, useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = "mario"
+    const [name, setName] = useState("mario");
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log("Hello, ninjas", e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log("Hello " + name, e.target);
+    const handleClick = () => {
+        setName("luigi");
+        setAge(30);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => {handleClickAgain("mario",e)}}>Click me handleClickAgain</button>
         </div>
      );
 }
