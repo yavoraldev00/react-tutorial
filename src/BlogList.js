9cf81153dfeddd9b305d99873c6b75f5e976847a
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-02 13:32:39 +0200

    tutorial 11 - props

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = ( { blogs, title }) => { // desctrucutre props in param field
+    // const blogs = props.blogs; // defining props
+    // const title = props.title;
+
+    return ( 
+        <div className="blog-list">
+            <h2>{ title }</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
