194230b6da57dfc3b9ae215496e6160ded99baf6
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-04 23:33:36 +0200

    tutorial 25 - route tutorials

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ( { blogs, title }) => { // desctrucutre props in param field
     // const blogs = props.blogs; // defining props
     // const title = props.title;
@@ -6,10 +8,12 @@ const BlogList = ( { blogs, title }) => { // desctrucutre props in param field
         <div className="blog-list">
             <h2>{ title }</h2>
             {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
+                <Link to={`/blogs/${blog.id}`}>
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by { blog.author }</p>
+                    </div>
+                </Link>
             ))}
         </div>
      );
