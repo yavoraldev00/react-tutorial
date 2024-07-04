07a68699be459420537cd8da36f6ddbc98d1f5f0
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-04 22:28:15 +0200

    tutorial 19 - handling errors

--- src/Home.js
+++ src/Home.js
@@ -4,33 +4,38 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [errMessage, setErrMessage] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
-    const [name, setName] = useState("mario");
-
     useEffect(() => {
         setTimeout(() => {
             fetch("http://localhost:8000/blogs") // fetch request to server
             .then((res) => {
+                if(!res.ok){ // error handling after connecting to server
+                    throw Error("Could not fetch data from resource") // creates error messages
+                }
                 return res.json();
             }).then((data) => {
                 console.log(data);
                 setBlogs(data);
                 setIsPending(false);
+                setErrMessage(null);
+            }).catch(err => { // error on encountered problem in connection
+                setIsPending(false);
+                setErrMessage(err.message);
             })
         }, 1500);
     }, []) // tirggers only on first build
 
     return ( 
         <div className="home">
+            {errMessage && <div>{errMessage}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs = {blogs} title = "Blogs" handleDelete = {handleDelete}/> /* conditional templating */}
-            <button onClick={() => {setName("luigi")}}>Change Name</button>
-            <p>{ name }</p>
         </div>
      );
 }
