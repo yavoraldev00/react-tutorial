74e8327004120bc9b9416d7858256e01ecb60bc7
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-04 23:03:30 +0200

    tutorial 21 - react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
