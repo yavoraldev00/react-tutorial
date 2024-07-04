27051979110cc819afb3ee9dd86f340c3f4cae8d
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-04 22:17:13 +0200

    tutorial 18 - Conditional Loading Messages

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -12,17 +13,21 @@ const Home = () => {
     const [name, setName] = useState("mario");
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogs") // fetch request to server
-        .then((res) => {
-            return res.json();
-        }).then((data) => {
-            console.log(data);
-            setBlogs(data);
-        })
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs") // fetch request to server
+            .then((res) => {
+                return res.json();
+            }).then((data) => {
+                console.log(data);
+                setBlogs(data);
+                setIsPending(false);
+            })
+        }, 1500);
     }, []) // tirggers only on first build
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs = {blogs} title = "Blogs" handleDelete = {handleDelete}/> /* conditional templating */}
             <button onClick={() => {setName("luigi")}}>Change Name</button>
             <p>{ name }</p>
