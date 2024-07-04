9114bee4286c195e28bc06e60a92bf1eb6854b51
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-04 23:25:41 +0200

    tutorial 24 - useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react"
 
+
 const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [errMessage, setErrMessage] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url) // fetch request to server
+            fetch(url, { signal: abortCont.signal }) // fetch request to server
             .then((res) => {
                 if(!res.ok){ // error handling after connecting to server
                     throw Error("Could not fetch data from resource") // creates error messages
@@ -19,10 +22,16 @@ const useFetch = (url) => {
                 setIsPending(false);
                 setErrMessage(null);
             }).catch(err => { // error on encountered problem in connection
-                setIsPending(false);
-                setErrMessage(err.message);
+                if(err.name === "AbortError"){
+                    console.log("Aborted error");
+                }else{
+                    setIsPending(false);
+                    setErrMessage(err.message);
+                }
             })
         }, 1500);
+
+        return () => { abortCont.abort(); };
     }, [url]) // tirggers only on first build
 
     return { data, isPending, errMessage };
