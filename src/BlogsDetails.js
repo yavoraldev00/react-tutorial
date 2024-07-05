e978af979eb8030926f9e494aefa409d68d84a28
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-05 19:16:59 +0200

    tutorial 31 - deleting blogs

--- src/BlogsDetails.js
+++ src/BlogsDetails.js
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, errorMessage, isPending } = useFetch("http://localhost:8000/blogs/" + id);
+    const history = useHistory();
+
+    const handleDelete = () => {
+        debugger;
+        fetch("http://localhost:8000/blogs/" + id, { // fetch request for current blog
+            method: "DELETE" // sends a delete request
+        }).then(() => {
+            history.push("/")
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -15,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by {blog.author}</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
