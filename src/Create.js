e24a2af418b7ea0699580dd8f608cbf7ca17ec84
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-05 19:04:58 +0200

    tutorial 29 - making POST request

--- src/Create.js
+++ src/Create.js
@@ -4,9 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("mario");
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch("http://localhost:8000/blogs", {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("new blog added");
+            setIsPending(false);
+        })
     }
 
     return ( 
@@ -31,7 +44,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
             </form>
         </div>
      );
