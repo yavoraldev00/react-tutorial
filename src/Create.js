ab9ad3765c0eb21a4b7d452be2f6b04baa99a3c7
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-05 18:49:43 +0200

    tutorial 27 - controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario");
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                type="text"
+                value = {title} // state value associated with input
+                onChange={(e) => {setTitle(e.target.value)}} // changes state to reflect element text
+                required
+                />
+                <label>Blog body:</label>
+                <textarea
+                value = {body}
+                onChange={(e) => {setBody(e.target.value)}}
+                required
+                ></textarea>
+                <label>Blog author:</label>
+                <select value={author} onChange={(e) => {setAuthor(e.target.value)}}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
